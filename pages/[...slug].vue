e1092bf0b37f280424126daf1b9969d5e4f81573
Thom Bruce <thom@thombruce.com>
2023-08-12 15:32:33 +0100

    simplify application logic for reusability

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+const route = useRoute()
+
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: route.path, where: [{ navigation: { $ne: false }, _dir: route.path.slice(1) }] }
+</script>
+
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #default="{ doc }">
+        <h1>{{ doc.title }}</h1>
+        <ContentRenderer :value="doc" />
+      </template>
+      <template #empty></template>
+    </ContentDoc>
+    <ContentList :query="query">
+      <template #default="{ list }">
+        <ul>
+          <li v-for="article in list" :key="article._path">
+            <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+          </li>
+        </ul>
+      </template>
+      <template #not-found></template>
+    </ContentList>
   </main>
 </template>
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <main>
-    <h1>The Definitive Edition</h1>
-    <p><em>The Definitive Edition</em> is a blog dedicated to documenting the best way to play classic games.</p>
-  </main>
-</template>
