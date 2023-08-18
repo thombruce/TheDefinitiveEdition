f6fdd3c3cd4f467c46da960c35649fea0c773840
Thom Bruce <thom@thombruce.com>
2023-08-19 00:49:42 +0100

    form and limit adjustments

--- components/content/SimpleSearchBar.vue
+++ components/content/SimpleSearchBar.vue
@@ -4,8 +4,8 @@ const query = ref('')
 const results = ref({})
 
 async function search() {
-  results.value = await useFetch('/.netlify/functions/games', { method: 'post', body: { query: query.value } })
-  console.log(results.value)
+  const response = await useFetch('/.netlify/functions/games', { method: 'post', body: { query: query.value } })
+  results.value = await JSON.parse(response.data)
 }
 </script>
 
@@ -13,5 +13,17 @@ async function search() {
   <input type="text" class="input input-bordered" @change="search()" v-model="query" />
   <div v-if="results">
     {{ results }}
+  </div>
+
+
+  <div class="dropdown mb-32">
+    <summary class="m-1 btn">
+      <input type="text" class="input input-bordered" @change="search()" v-model="query" />
+    </summary>
+    <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+      <li v-for="result in results">
+        <a>{{ result.name }}</a>
+      </li>
+    </ul>
   </div>
 </template>
