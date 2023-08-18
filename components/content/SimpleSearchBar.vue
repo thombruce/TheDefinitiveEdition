4a091c865a3f1b04e3bf6ad394e41c687565e615
Thom Bruce <thom@thombruce.com>
2023-08-18 23:02:33 +0100

    add experimental search page

---
+++ components/content/SimpleSearchBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+const query = ref('foo')
+
+const { data: results } = {}
+
+async function search() {
+  results = await useFetch('/.netlify/functions/igdb/games', { body: { query } })
+  console.log(results)
+}
+</script>
+
+<template>
+  <input type="text" class="input input-bordered" @change="search()" v-model="query" />
+  <span>{{ query }}</span>
+  <div v-if="results">
+    {{ results }}
+  </div>
+</template>
---
+++ netlify/functions/igdb/games.ts
@@ -0,0 +1,25 @@
+const API_ENDPOINT = "https://api.igdb.com/v4/games"
+
+exports.handler = async (event: any, context: any, callback: any) => {
+  try {
+    var body = JSON.parse(event.body)
+    const response = await fetch(API_ENDPOINT,
+      { method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Client-ID': `${process.env.TWITCH_CLIENT_ID}`,
+          'Authorization': `Bearer ${process.env.TWITCH_AUTH_TOKEN}`,
+        },
+        body: `fields name; search "${body.query}"; where version_parent = null; limit 10;`
+      }
+    )
+    const data = await response.json()
+    return { statusCode: 200, body: JSON.stringify({ data }) }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    }
+  }
+}
