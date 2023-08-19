5a5e9caaf59d2ade8f8111db3f847da6f8dcb706
Thom Bruce <thom@thombruce.com>
2023-08-19 01:40:57 +0100

    simplify

--- components/content/SimpleSearchBar.vue
+++ components/content/SimpleSearchBar.vue
@@ -5,10 +5,9 @@ const results = ref({})
 
 async function search() {
   await useFetch('/.netlify/functions/games', { method: 'post', body: { query: query.value } })
-    .then((response) => {
-      console.log(response.data)
-      results.value = JSON.parse(JSON.stringify(response.data))
-      console.log(results.value)
+    .then(({ data }) => {
+      console.log(data)
+      results.value = data
     })
 }
 </script>
