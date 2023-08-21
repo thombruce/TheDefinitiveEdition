6ec76e5ad1ced563dabe11432fa9a5e68aea371e
Thom Bruce <thom@thombruce.com>
2023-08-21 20:18:29 +0100

    revert to native fetch

--- components/content/SimpleSearchBar.vue
+++ components/content/SimpleSearchBar.vue
@@ -26,8 +26,8 @@ const treated = computed(() => {
 const isActive = ref(false)
 
 const search = _debounce(async () => {
-  const response = await $fetch('/.netlify/functions/games', { method: 'POST', body: JSON.stringify({ query: query.value }) })
-  results.value = response
+  const response = await fetch('/.netlify/functions/games', { method: 'POST', body: JSON.stringify({ query: query.value }) })
+  results.value = await response.json()
   console.log(results.value)
 }, 250, { 'maxWait': 500 })
 
