e8e787db8e68b6badcc8cc4da2d91d8d7447ec8b
Thom Bruce <thom@thombruce.com>
2023-08-26 02:37:00 +0100

    add custom default layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,18 @@ export default defineNuxtConfig({
   runtimeConfig: {
     twitchClientID: process.env.CLIENT_ID,
     twitchAuthToken: process.env.AUTH_TOKEN
+  },
+  content: {
+    documentDriven: {
+      globals: {
+        theme: {
+          where: {
+            _id: 'content:_theme.yml'
+          },
+          without: ['_']
+        }
+      },
+      layoutFallbacks: ['theme'],
+    },
   }
 })
