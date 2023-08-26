7e2d6464f5de2d96b0621ffc74d19c7943619eeb
Thom Bruce <thom@thombruce.com>
2023-08-26 15:07:57 +0100

    essential form changes

--- components/content/Contribute.vue
+++ components/content/Contribute.vue
@@ -1,27 +1,27 @@
 <script setup>
 const { hostname } = useAppConfig()
 const route = useRoute()
+
+const title = ref('')
+
+const slug = computed(() => {
+  return _kebabCase(title.value)
+})
 </script>
 
 <template>
-<form method="POST" action="https://staticman.thombruce.com/v3/entry/github/thombruce/thedefinitiveedition/main/articles">
+<TntForm method="POST" action="https://staticman.thombruce.com/v3/entry/github/thombruce/thedefinitiveedition/main/articles">
   <!-- hidden -->
   <input name="options[redirect]" type="hidden" :value="`${hostname}${route.path}`" />
 
   <!-- visible -->
-  <div class="form-control">
-    <label for="contributionName" class="label"><span class="label-text">Name</span></label>
-    <input id="contributionName" name="fields[name]" type="text" class="input input-bordered" />
-  </div>
-  <div class="form-control">
-    <label for="contributionEmail" class="label"><span class="label-text">E-mail</span></label>
-    <input id="contributionEmail" name="fields[email]" type="email" class="input input-bordered" />
-    <label for="contributionEmail" class="label"><span class="label-text-alt">This won't be shown.</span></label>
-  </div>
-  <div class="form-control">
-    <label for="contributionTitle" class="label"><span class="label-text">Title</span></label>
-    <input id="contributionTitle" name="fields[title]" type="text" class="input input-bordered" />
-  </div>
+  <TntInput id="contributionName" label="Name" name="fields[name]" type="text" />
+
+  <TntInput id="contributionEmail" label="Email" name="fields[email]" type="email" hint="This won't be shown." />
+
+  <TntInput id="contributionTitle" label="Title" name="fields[title]" type="text" v-model="title" />
+  <input name="fields[slug]" type="hidden" :value="slug" />
+
   <div class="form-control">
     <label for="contributionGame" class="label">
       <span class="label-text">Game</span>
@@ -30,16 +30,9 @@ const route = useRoute()
     <SimpleSearchBar inputid="contributionGame" inputname="fields[game]" inputclass="input input-bordered" />
     <!-- TODO: Slugify for use as Dir name -->
   </div>
-  <div class="form-control">
-    <label for="contributionContent" class="label">
-      <span class="label-text">Content</span>
-      <span class="label-text-alt">Supports Markdown</span>
-    </label>
-    <textarea id="contributionContent" name="fields[content]" class="textarea textarea-bordered"></textarea>
-  </div>
 
-  <div class="mt-5">
-    <button type="submit" class="btn btn-primary">Submit!</button>
-  </div>
-</form>
+  <TntTextarea id="contributionContent" label="Content" name="fields[content]" labelAlt="Supports Markdown" />
+
+  <TntButton />
+</TntForm>
 </template>
